bc33d72cb412f46461dc32b11315340a10be1236
Jung seo young <sju06071@gmail.com>
2022-04-16 23:37:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +17,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
